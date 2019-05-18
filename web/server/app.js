a54c09110936ea39b0363a92feac16103a0d2281
xepps <kristian@xepps.com>
2019-05-18 22:42:16 +0100

    docker networking learned

--- web/server/app.js
+++ web/server/app.js
@@ -1,14 +1,39 @@
 const Koa = require('koa');
 
-const app = new Koa();
 const serve = require('koa-static');
 const mount = require('koa-mount');
+const proxyPass = require('@junyiz/koa-proxy-pass');
 
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-
 const readFile = util.promisify(fs.readFile);
+
+const apiHost = process.env.API_HOST || 'localhost';
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log((`${ctx.method} ${ctx.url} - ${ms}`));
+});
+
+app.on('error', (err) => {
+  console.error('Server Error', err);
+});
+
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  return next();
+});
+
+app.use(proxyPass({
+  host: `http://${apiHost}:3000`,
+  map: p => p.replace('/api', ''),
+  match: /^\/api\//,
+}));
 
 app.use(mount('/', serve(path.join(__dirname, '../dist'))));
 
