21b26b63b06d018e608e345a08b79867773b33f5
Richard Walkden <richard.walkden@netacea.com>
2019-06-12 12:07:02 +0100

    Super test search

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,13 +1,46 @@
 const supertest = require('supertest')
 const tape = require('tape')
 const sinon = require('sinon')
+var proxyquire = require('proxyquire').noCallThru();
 
-const app = require('../src/app')
+const newApp = () => {
+    return proxyquire('../src/app', {
+        'elasticsearch': {
+            'Client': function () {
+                return {
+                    'indices': {
+                        'create': sinon.stub().resolves({}),
+                        'exists': sinon.stub().resolves({}),
+                        'delete': sinon.stub().resolves({}),
+                        'putMapping': sinon.stub().resolves({}),
+                    },
+                    'search': sinon.stub().resolves({})
+                }
+            },
+            '@global': true
+        }
+    })
+}
 
-tape('app route :: /all', async (t) => {
+tape('app route :: /search', async (t) => {
+    const app = newApp()
     await supertest(app)
         .get('/all')
         .expect('Content-Type', /json/)
         .expect(200)
+
+    app.close()
+    t.end()
+})
+
+tape('app route :: /all', async (t) => {
+    const app = newApp()
+    await supertest(app)
+        .get('/all')
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+    app.close()
+    t.end()
 })
 
