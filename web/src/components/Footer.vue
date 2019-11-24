b596fad5137155f9864eb146adaa106993b58bf3
xepps <kristian@xepps.com>
2019-11-24 15:51:51 +0000

    adds footer

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="site-footer">
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.site-footer {
+  border-top: #9d0a0e 3px solid;
+  width: 100%;
+  height: 200px;
+  background: #313131;
+}
+</style>
